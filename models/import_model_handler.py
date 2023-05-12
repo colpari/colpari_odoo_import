fe255915333cfbcd63a126de9efbd2da4bc72c5c
Frank Fricke <frank.fricke@colpari.cx>
2023-05-12 12:37:30 +0200

    debug

--- models/import_model_handler.py
+++ models/import_model_handler.py
@@ -738,8 +738,8 @@ class ImportModelHandler():
 		for remoteId, dataToUpdate in objectSetMapped.items():
 			localId = self.idMap[remoteId]
 			localObject = localObjects[localId]
-			if self.modelName == 'res.partner.bank':
-				_logger.info("{} updating {} with {}".format(self.modelName, localObject, dataToUpdate))
+			# if self.modelName == 'res.partner.bank':
+			# 	_logger.info("{} updating {} with {}".format(self.modelName, localObject, dataToUpdate))
 			try:
 				#FIXME: evaluate if we want this
 				# #if self.__wouldBeChangedBy(localObject, dataToUpdate):
